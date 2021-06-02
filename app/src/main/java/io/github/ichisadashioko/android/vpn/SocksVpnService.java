c6ec3e040690039e40a1417a1b6dcb480f87ee87
shioko <ichisadashioko@gmail.com>
2021-06-02 09:56:56 +0000

    create a new VpnService

---
+++ app/src/main/java/io/github/ichisadashioko/android/vpn/SocksVpnService.java
@@ -0,0 +1,23 @@
+package io.github.ichisadashioko.android.vpn;
+
+import android.content.Intent;
+import android.net.VpnService;
+
+public class SocksVpnService extends VpnService{
+    public static final String TAG = SocksVpnService.class.getName();
+
+    public boolean isRunning = false;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        if(intent == null){
+            return START_STICKY;
+        }
+
+        if(isRunning){
+            return START_STICKY;
+        }
+
+        return START_STICKY;
+    }
+}
